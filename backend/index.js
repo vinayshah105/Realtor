be98e0090838f283e47d031d868f1824ce322700
Vinay Shah <shahvinay1005.sv@gmail.com>
2023-10-01 17:03:48 -0400

    changes in mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Server is connected with MongoDB');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is Running on port 3000')
+  console.log('Server is Running on port 3000');
 });
